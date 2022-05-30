ccce09a4bef950cc8c543874030a4f15ff8c2824
Igor Kovalchuk <kowal4uck.gosha@yandex.ru>
2022-05-30 19:25:35 +0300

    full version

---
+++ Client/Spamer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Spamer
+    {
+        private readonly Logger logger_;
+        private readonly StringGenerator generator_;
+        public Spamer(Logger logger, StringGenerator generator)
+        {
+            this.logger_ = logger;
+            this.generator_ = generator;
+        }
+
+        public void Spam()
+        {
+            logger_.Log(generator_.RandomString(10));
+        }
+    }
+}
